4cc2f13bcc8563bbef20a7fb01d5bfd2fa86dc52
Ali Hassan Faruk Asif <asifkhondaker004@gmail.com>
2020-07-14 13:40:12 +0600

    Updates

---
+++ precache-manifest.d249af2c5bbfb4f6d90dc76c9eab21dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c235c8660b07ca1af537093e7acf0f7",
+    "url": "/React-router-basic/index.html"
+  },
+  {
+    "revision": "ad202cc5f59b0f11bf43",
+    "url": "/React-router-basic/static/css/main.981f48c0.chunk.css"
+  },
+  {
+    "revision": "14df9729bd7c300c317b",
+    "url": "/React-router-basic/static/js/2.65ce2d7f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-router-basic/static/js/2.65ce2d7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad202cc5f59b0f11bf43",
+    "url": "/React-router-basic/static/js/main.ae71420e.chunk.js"
+  },
+  {
+    "revision": "7bf3ac3e7b04a04f60bb",
+    "url": "/React-router-basic/static/js/runtime-main.2bf287fc.js"
+  }
+]);
